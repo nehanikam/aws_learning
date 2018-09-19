f8f100508bbd4850f15b848f9d1b55f4a3e169da
neha nikam <nikam.neha93@gmail.com>
2018-09-20 03:15:47 +0530

    Added region to the s3 download command

--- boot.sh
+++ boot.sh
@@ -14,7 +14,7 @@ service httpd start
 # ensure it runs every time
 chkconfig httpd on
 
-# download file from s3
-aws s3 cp s3://firststaticwebsite-s3bucket/ /var/www/html/
+# download file from s3 into the region in which this ec2 instance is
+aws s3 cp s3://firststaticwebsite-s3bucket/ /var/www/html/ --region us-west-2
 
 echo ".... server installation finished"
